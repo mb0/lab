f7b0351b15f50489df3ec618fb349c0cfe9dc53e
Martin Schnabel <mb0@mb0.org>
2013-02-23 04:40:02 +0100

    adds go file and package detection

---
+++ golab/gosrc/gosrc.go
@@ -0,0 +1,136 @@
+package gosrc
+
+import (
+	"fmt"
+	"path/filepath"
+	"sync"
+	"time"
+
+	"github.com/mb0/lab/ws"
+)
+
+var (
+	FlagGo  uint32 = 1 << 16
+	FlagSrc uint32 = 1 << 17
+	FlagPkg        = FlagGo | FlagSrc
+)
+
+type Pkg struct {
+	sync.Mutex
+	Id               ws.Id
+	ImportPath, Name string
+}
+
+type Src struct {
+	sync.RWMutex
+	srcids []ws.Id
+	pkgs   map[ws.Id]*Pkg
+	queue  *ws.Throttle
+	rmchan chan *ws.Res
+}
+
+func New(srcids []ws.Id) *Src {
+	return &Src{
+		srcids: srcids,
+		pkgs:   make(map[ws.Id]*Pkg),
+		queue: &ws.Throttle{
+			Tickers: make(chan *time.Ticker, 1),
+			Ticks:   time.Second / 2,
+		},
+		rmchan: make(chan *ws.Res),
+	}
+}
+
+func (s *Src) Filter(r *ws.Res) bool {
+	if r.Flag&ws.FlagDir == 0 {
+		if filepath.Ext(r.Name) == ".go" {
+			r.Parent.Flag |= FlagSrc
+			r.Flag |= FlagSrc
+		}
+		return false
+	}
+	if r.Parent.Flag&FlagGo != 0 {
+		if r.Name[0] != '_' {
+			r.Flag |= FlagGo
+		}
+		return false
+	}
+	if r.Name == "pkg" || r.Name == "src" {
+		for _, id := range s.srcids {
+			if r.Id == id {
+				r.Flag |= FlagGo
+				break
+			}
+		}
+	}
+	return false
+}
+
+func (s *Src) Handle(op ws.Op, r *ws.Res) {
+	if r.Flag&FlagSrc == 0 {
+		return
+	}
+	if r.Flag&FlagGo != 0 {
+		switch op & ws.WsMask {
+		case ws.Change:
+			s.queue.Add(r)
+		case ws.Remove:
+			s.rmchan <- r
+		}
+		return
+	}
+	if op&ws.FsMask != 0 {
+		s.queue.Add(r.Parent)
+	}
+	return
+}
+func (s *Src) Run() {
+	var timeout <-chan time.Time
+	for {
+		select {
+		case t := <-s.queue.Tickers:
+			if timeout = nil; t != nil {
+				timeout = t.C
+			}
+		case <-timeout:
+			for _, r := range s.queue.Work() {
+				s.change(r)
+			}
+		case r := <-s.rmchan:
+			s.queue.Delete(r)
+			s.remove(r)
+		}
+	}
+}
+func importPath(r *ws.Res, buf []byte) []byte {
+	if r.Parent.Parent.Flag&FlagGo != 0 {
+		buf = importPath(r.Parent, buf)
+		buf = append(buf, '/')
+	}
+	return append(buf, []byte(r.Name)...)
+}
+func (s *Src) change(r *ws.Res) {
+	s.Lock()
+	defer s.Unlock()
+	p, ok := s.pkgs[r.Id]
+	if !ok {
+		p = &Pkg{Id: r.Id, ImportPath: string(importPath(r, make([]byte, 0, 64)))}
+		s.pkgs[r.Id] = p
+		fmt.Printf("pkg add %s\n", p.ImportPath)
+	} else {
+		fmt.Printf("pkg update %s\n", p.ImportPath)
+	}
+	// scan ?
+	// enqueue build and test run
+}
+func (s *Src) remove(r *ws.Res) {
+	s.Lock()
+	defer s.Unlock()
+	p, ok := s.pkgs[r.Id]
+	if !ok {
+		return
+	}
+	// clean up p
+	delete(s.pkgs, p.Id)
+	fmt.Printf("pkg remove %s\n", p.ImportPath)
+}
--- golab/main.go
+++ golab/main.go
@@ -3,21 +3,37 @@ package main
 import (
 	"flag"
 	"fmt"
-	"github.com/mb0/lab/ws"
+	"go/build"
 	"os"
 	"os/signal"
 	"path/filepath"
+
+	"github.com/mb0/lab/golab/gosrc"
+	"github.com/mb0/lab/ws"
 )
 
 var paths = flag.String("paths", "", "paths to watch. defaults to cwd")
 
+var src *gosrc.Src
 func filter(r *ws.Res) bool {
 	if len(r.Name) > 0 && r.Name[0] == '.' {
 		return true
 	}
-	return false
+	return src.Filter(r)
 }
-
+func handler(op ws.Op, r *ws.Res) {
+	if r.Flag&ws.FlagIgnore != 0 {
+		return
+	}
+	src.Handle(op, r)
+}
+func ids(paths []string) []ws.Id {
+	ids := make([]ws.Id, 0, len(paths))
+	for _, p := range paths {
+		ids = append(ids, ws.NewId(p))
+	}
+	return ids
+}
 func main() {
 	flag.Parse()
 	var err error
@@ -28,17 +44,17 @@ func main() {
 			return
 		}
 	}
-	dirs := filepath.SplitList(*paths)
+	// TODO create active working set
+	_ = ids(filepath.SplitList(*paths))
+	dirs := build.Default.SrcDirs()
+	src = gosrc.New(ids(dirs))
+	go src.Run()
 	fmt.Printf("starting lab for %v\n", dirs)
 	w := ws.New(ws.Config{
-		CapHint: 1000,
+		CapHint: 8000,
 		Watcher: ws.NewInotify,
 		Filter:  filter,
-		Handler: func(op ws.Op, r *ws.Res) {
-			if r.Flag&ws.FlagIgnore == 0 {
-				fmt.Printf("op %x for %s\n", op, r.Path())
-			}
-		},
+		Handler: handler,
 	})
 	defer w.Close()
 	for _, dir := range dirs {

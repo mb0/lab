f693353a1b372e036c591b3e583006dd52e1217f
Martin Schnabel <mb0@mb0.org>
2013-02-23 04:26:51 +0100

    adds resource queue and throttle utility

--- ws/util.go
+++ ws/util.go
@@ -3,6 +3,7 @@ package ws
 import (
 	"runtime"
 	"sync"
+	"time"
 )
 
 func MountAll(w *Ws, dirs []string) []error {
@@ -29,4 +30,73 @@ func MountAll(w *Ws, dirs []string) []error {
 		}
 	}
 	return nil
+}
+
+type Queue struct {
+	sync.Mutex
+	queue []*Res
+	batch []*Res
+}
+
+func (q *Queue) del(r *Res) {
+	for i, qr := range q.queue {
+		if qr.Id == r.Id {
+			q.queue = append(q.queue[:i], q.queue[i+1:]...)
+			return
+		}
+	}
+	for i, qr := range q.batch {
+		if qr.Id == r.Id {
+			q.batch = append(q.batch[:i], q.batch[i+1:]...)
+			return
+		}
+	}
+}
+func (q *Queue) Delete(r *Res) {
+	q.Lock()
+	defer q.Unlock()
+	q.del(r)
+}
+func (q *Queue) Add(r *Res) {
+	q.Lock()
+	defer q.Unlock()
+	q.del(r)
+	q.queue = append(q.queue, r)
+}
+func (q *Queue) Work() []*Res {
+	q.Lock()
+	defer q.Unlock()
+	res := make([]*Res, len(q.batch))
+	copy(res, q.batch)
+	q.queue, q.batch = q.batch[:0], q.queue
+	return res
+}
+
+type Throttle struct {
+	sync.Mutex
+	Queue
+	Tickers chan *time.Ticker
+	Ticks   time.Duration
+	ticker  *time.Ticker
+}
+
+func (q *Throttle) Add(r *Res) {
+	q.Lock()
+	defer q.Unlock()
+	q.Queue.Add(r)
+	if q.ticker == nil {
+		q.ticker = time.NewTicker(q.Ticks)
+		q.Tickers <- q.ticker
+	}
+}
+func (q *Throttle) Work() []*Res {
+	q.Lock()
+	defer q.Unlock()
+	res := q.Queue.Work()
+	if len(q.batch) == 0 && q.ticker != nil {
+		q.ticker.Stop()
+		q.ticker = nil
+		q.Tickers <- nil
+	}
+	return res
 }

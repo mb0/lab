becf3bfd56e42822407e7c427dea47b1bfe63bc5
Martin Schnabel <mb0@mb0.org>
2013-02-27 04:14:28 +0100

    uses -test.timeout instead of time.After for test runs. "go install" and "go test -c" can be trusted not to hang or deadlock.

--- golab/gosrc/install.go
+++ golab/gosrc/install.go
@@ -32,7 +32,7 @@ func Install(pkg *Pkg) *Report {
 		return r
 	}
 	r.Start = time.Now()
-	waitfor(cmd, r, time.Second)
+	r.Err = cmd.Wait()
 	return r
 }
 
@@ -52,17 +52,18 @@ func Test(pkg *Pkg) *Report {
 		return r
 	}
 	r.Start = time.Now()
-	waitfor(cmd, r, time.Second)
+	r.Err = cmd.Wait()
 	if r.Err != nil {
 		return r
 	}
 	_, binary := filepath.Split(r.Path)
 	binary += testexe
-	cmd = rcmd(r, filepath.Join(tmp, binary), binary, "-test.v", "-test.short")
+	cmd = rcmd(r, filepath.Join(tmp, binary), binary, "-test.v", "-test.short", "-test.timeout=3s")
+	cmd.Dir = pkg.Dir
 	if r.Err = cmd.Start(); r.Err != nil {
 		return r
 	}
-	waitfor(cmd, r, 3*time.Second)
+	r.Err = cmd.Wait()
 	return r
 }
 
@@ -73,19 +74,5 @@ func rcmd(r *Report, args ...string) *exec.Cmd {
 		Dir:    os.TempDir(),
 		Stdout: &r.out,
 		Stderr: &r.err,
-	}
-}
-
-func waitfor(cmd *exec.Cmd, r *Report, timeout time.Duration) {
-	done := make(chan error)
-	go func() {
-		done <- cmd.Wait()
-	}()
-	select {
-	case err := <-done:
-		r.Err = err
-	case <-time.After(timeout):
-		cmd.Process.Kill()
-		r.Err = ErrTimeout
 	}
 }

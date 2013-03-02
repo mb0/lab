34efc97f5d9da16c805dea1409d3d36f5db839c7
Martin Schnabel <mb0@mb0.org>
2013-03-02 01:54:48 +0100

    moves client report code.

---
+++ golab/static/view/report.js
@@ -0,0 +1,63 @@
+/*
+Copyright 2013 Martin Schnabel. All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+*/
+define(["base", "conn"], function(base, conn) {
+
+var Report = Backbone.Model.extend({
+	getresult: function() {
+		var src = this.get("Src").Result, test = this.get("Test").Result;
+		if (src && src.Err) return src;
+		if (test) return test;
+		return src
+	},
+	getstatus: function(res) {
+		return !(res && res.Err) ? "ok" : "fail";
+	},
+	getoutput: function(res) {
+		if (!res) return "";
+		var out = (res.Stdout || "") + (res.Stderr || "");
+		return out.replace(/(^(#.*|\S)\n|\n#[^\n]*)/g, "");
+	},
+});
+
+var Reports = Backbone.Collection.extend({model:Report});
+
+var ReportListItem = base.ListItemView.extend({
+	template: _.template([
+		'<% var res = getresult(); var stat = getstatus(res), o = getoutput(res) %>',
+		'<div class="report <%- stat %>">',
+		'<header>',
+		'<span class="status"><%- stat.toUpperCase() %></span> ',
+		'<span class="mode"><%= res && res.Mode || "" %></span> ',
+		'<%= get("Path") %> <%= res && res.Err || "" %>',
+		'</header>',
+		'<% if (o) { %><pre><%= o %></pre><% } %>',
+		'</div>',
+	].join('')),
+});
+
+var ReportList = base.ListView.extend({
+	itemView: ReportListItem
+});
+
+var ReportView = Backbone.View.extend({
+	tagName: "section",
+	initialize: function() {
+		this.reports = new Reports();
+		this.listview = new ReportList({collection:this.reports});
+		this.listenTo(conn, "msg:report msg:reports", this.addReport);
+		this.render();
+	},
+	render: function() {
+		this.$el.append(this.listview.render().$el);
+		return this;
+	},
+	addReport: function(data) {
+		this.reports.add(data);
+	}
+});
+
+return {View: ReportView};
+});

11f2396a336bd88f59bf7189d72e779032bbc903
Martin Schnabel <mb0@mb0.org>
2013-03-23 00:16:54 +0100

    adds cleaner ace configuration.

---
+++ golab/static/view/ace.js
@@ -0,0 +1,77 @@
+/*
+Copyright 2013 Martin Schnabel. All rights reserved.
+Use of this source code is governed by a BSD-style
+license that can be found in the LICENSE file.
+*/
+define(["require", "ace/lib/event", "ace/editor", "ace/edit_session", "ace/undomanager", "ace/virtual_renderer", "ace/multi_select"],
+function(require, event) {
+
+function createRenderer(container, theme) {
+	var R = require("ace/virtual_renderer").VirtualRenderer;
+	var r = new R(container, theme || {
+		cssClass: "ace_lab",
+		isDark:true,
+	});
+	r.setAnimatedScroll(true);
+	r.setShowGutter(true);
+	r.setShowPrintMargin(true);
+	r.setPrintMarginColumn(120);
+	r.setHScrollBarAlwaysVisible(false);
+	return r;
+}
+
+function createSession(value, mode) {
+	var S = require("ace/edit_session").EditSession;
+	var U = require("ace/undomanager").UndoManager;
+	var s = new S(value, mode);
+	s.setUndoManager(new U());
+	s.setUseSoftTabs(false);
+	s.setTabSize(8);
+	return s;
+}
+
+function createEditor(renderer, session, multi) {
+	var E = require("ace/editor").Editor;
+	var M = require("ace/multi_select").MultiSelect;
+	var e = new E(renderer, session);
+	if (multi) new M(e);
+	e.setScrollSpeed(5);
+	e.setDragDelay(3000);
+	e.setHighlightActiveLine(true);
+	e.setHighlightSelectedWord(true);
+
+	var r = e.resize.bind(e, null);
+	event.addListener(window, "resize", r);
+	e.on("destroy", function() {
+		event.removeListener(window, "resize", r);
+	});
+	return e;
+}
+
+
+function previousToken(session, pos, type, ignored) {
+	var r = pos.row, i = pos.column
+	var toks, tok, skip = _.isArray(ignored);
+	for (toks = null; i >= 0 || r > 0; i--) {
+		if (toks === null || i < 0) {
+			toks = session.getTokens(i < 0 ? --r : r);
+			i = toks.length -1;
+		}
+		if (i < 0)
+			continue;
+		tok = toks[i];
+		if (tok.type == type)
+			return tok;
+		if (!skip || !_.contains(ignored, tok.type))
+			return null;
+	}
+	return null;
+}
+
+return {
+	createRenderer: createRenderer,
+	createSession:  createSession,
+	createEditor:   createEditor,
+	previousToken:  previousToken,
+};
+});

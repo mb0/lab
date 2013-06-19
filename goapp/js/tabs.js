2a9a5cdcef68b267d6bc00a4e0ce80304c3bd430
Martin Schnabel <mb0@mb0.org>
2013-06-19 12:10:33 +0200

    adds active tab style

--- goapp/js/tabs.js
+++ goapp/js/tabs.js
@@ -4,19 +4,33 @@ Use of this source code is governed by a BSD-style
 license that can be found in the LICENSE file.
 */
 angular.module("goapp.tabs", [])
-.run(function($rootScope) {
-	$rootScope.tabs = [
-		{link:"#/about", name:"about"},
-		{link:"#/report", name:"report"},
-		{link:"#/file/a/b", name:"a/b", close: true},
-	];
+.run(function($rootScope, $route, $location) {
+	var tabs = $rootScope.tabs = {list:[]};
+	for (var path in $route.routes) {
+		var r = $route.routes[path];
+		if (!r.tabs) continue;
+		for (var link in r.tabs) {
+			var tab = r.tabs[link];
+			tab.link = link;
+			tabs.list.push(tab);
+		}
+	}
+	$rootScope.$on("$routeChangeSuccess", function(e, cur){
+		if (tabs.activeTab) {
+			tabs.activeTab.active = false;
+		}
+		tabs.activeTab = cur.$$route.tabs[$location.path()];
+		if (tabs.activeTab) {
+			tabs.activeTab.active = true;
+		}
+	});
 })
 .directive("tabs", function() {
 	return {
 		restrict: "EA",
 		template: [
-			'<ul><li ng-repeat="tab in tabs" ng-class="{active: tab.active}">',
-			'<a href="{{ tab.link }}">{{ tab.name }}</a>',
+			'<ul><li ng-repeat="tab in tabs.list" ng-class="{active: tab.active}">',
+			'<a href="#{{ tab.link }}" ng-bind-html-unsafe="tab.name"></a>',
 			'</li></ul>',
 		].join(""),
 	};

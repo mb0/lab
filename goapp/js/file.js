eeb682fb277d1869cf85424c0875f9605124766d
Martin Schnabel <mb0@mb0.org>
2013-06-19 13:14:26 +0200

    shows files in folders

--- goapp/js/file.js
+++ goapp/js/file.js
@@ -11,12 +11,37 @@ angular.module("goapp.file", ["goapp.conn"])
 	if ($location.hash().match(/^L\d+$/)) {
 		line = parseInt($location.hash().slice(1), 10);
 	}
-	$scope.file = {path: path, line: line};
+	$scope.fileIcon = function(child) {
+		if (!child.IsDir) {
+			return 'icon-file-alt';
+		}
+		return 'icon-folder-open-alt';
+	};
+	$scope.childIcon = function(child) {
+		if (!child.IsDir) {
+			return 'icon-file-alt';
+		}
+		return 'icon-folder-close-alt';
+	};
+	$scope.fileHeader = function(file) {
+		var path = file.Path;
+		if (path[0] == "/") path = path.substr(1);
+		var i, l = 0, parts = path.split("/");
+		var result = ['<i class="'+(file.IsDir ? 'icon-folder-open-alt' : 'icon-file-alt')+'"></i> '];
+		for (i=0; i < parts.length; i++) {
+			l += parts[i].length;
+			result.push('<a href="#/file/'+path.substr(0,l++)+'">/'+parts[i]+'</a>');
+		}
+		return result.join("");
+	};
 	var dereg = $scope.$on("conn.msg", function(e, msg) {
 		if (msg.Head == "stat" && msg.Data.Path == path) {
-			console.log("loaded "+path, msg.Data);
+			$scope.file = msg.Data;
+			$scope.$digest();
 		} else if (msg.Head == "stat.err" && msg.Data.Path == path) {
-			console.log("error: path "+path +" "+ msg.Data.Error, msg.Data);
+			$scope.file = msg.Data;
+			$scope.$digest();
+			$scope.file.error = "error: path "+path +" "+ msg.Data.Error;
 		}
 	});
 	conn.send("stat", path);
@@ -28,7 +53,14 @@ angular.module("goapp.file", ["goapp.conn"])
 		replace: true,
 		controller: "FileCtrl",
 		template: [
-			'<div class="file">path: {{ file.path }}, line {{ file.line }}</div>',
+			'<div class="file" ng-show="file">',
+			'<div ng-bind-html-unsafe="fileHeader(file)"></div>',
+			'<div ng-show="file.Error">error: path {{file.Path}} {{file.Error}}</div>',
+			'<ul ng-show="file.IsDir"><li ng-repeat="child in file.Children">',
+			'<a href="#/file{{ file.Path }}/{{ child.Name }}"><i class="{{childIcon(child)}}"></i> {{child.Name}}</a>',
+			'</li></ul>',
+			'<div ng-show="!file.IsDir">editor here</div>',
+			'</div>',
 		].join(""),
 	};
 });
